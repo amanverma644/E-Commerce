9b1dd820c364a093110334ab59e7bbe7b6d3ebed
Aman Verma <vaman644@gmail.com>
2021-11-14 00:05:57 +0530

    connecting to database and adding colors to console

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import colors from 'colors'
+import connectDB from './config/db.js'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,7 @@ const PORT = process.env.PORT | 5000
 
 app.listen(
   5000,
-  console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`)
+  console.log(
+    `Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`.yellow.bold
+  )
 )
