df815995e8a6d424fe5392df1dc8aca104065927
Aman Verma <vaman644@gmail.com>
2021-11-17 11:51:30 +0530

    Fetching products from the database

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const product = await Product.find({})
+    res.json(product)
+  })
+)
+
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product)
+    } else {
+      res.status(404).json({ message: 'Product not found' })
+    }
+  })
+)
+
+export default router
