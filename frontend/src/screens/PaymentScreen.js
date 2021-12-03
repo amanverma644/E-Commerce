5cf33b6a8423986e98df3b58da3e621ba2db36ce
Aman Verma <vaman644@gmail.com>
2021-12-03 22:42:05 +0530

    Payment screen and save payment method

---
+++ frontend/src/screens/PaymentScreen.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Form, Button, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+import { savePaymentMethod } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
+
+const PaymentScreen = () => {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const cart = useSelector((state) => state.cart)
+  const { shippingAddress } = cart
+
+  if (!shippingAddress) {
+    navigate('/shipping')
+  }
+
+  const [paymentMethod, setPaymentMethod] = useState('PayPal')
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    dispatch(savePaymentMethod(paymentMethod))
+    navigate('/placeorder')
+  }
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 step3 />
+      <h1>Payment Method</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group>
+          <Form.Label as="legend">Select Method</Form.Label>
+          <Col>
+            <Form.Check
+              type="radio"
+              label="PayPal or Credit Card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+            <Form.Check
+              type="radio"
+              label="Paytm"
+              id="Paytm"
+              name="paymentMethod"
+              value="Paytm"
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></Form.Check>
+          </Col>
+        </Form.Group>
+        <Button type="submit" variant="primary" className="mt-3">
+          Continue
+        </Button>
+      </Form>
+    </FormContainer>
+  )
+}
+
+export default PaymentScreen
