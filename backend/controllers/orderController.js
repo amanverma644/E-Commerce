cf5f00ef387b2f4d9fd70ac562bf32c2856ddcea
Aman Verma <vaman644@gmail.com>
2021-12-04 09:43:26 +0530

    order controller and route

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+import asyncHandler from 'express-async-handler'
+import Order from '../models/orderModel.js'
+
+// @desc    Create a new order
+// @route   POST /api/orders
+// @access  Private
+const addOrderItems = asyncHandler(async (req, res) => {
+  const {
+    orderItems,
+    shippingAddress,
+    paymentMethod,
+    itemsPrice,
+    shippingPrice,
+    texPrice,
+    totalPrice,
+  } = req.body
+
+  if (orderItems && orderItems.length === 0) {
+    res.status(400)
+    throw new Error('No order items')
+    return
+  } else {
+    const order = new Order({
+      orderItems,
+      user: req.user._id,
+      shippingAddress,
+      paymentMethod,
+      itemsPrice,
+      shippingPrice,
+      texPrice,
+      totalPrice,
+    })
+    const createOrder = await order.save()
+    res.status(201).json(createOrder)
+  }
+})
+
+export { addOrderItems }
