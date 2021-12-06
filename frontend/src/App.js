6fc58242a76cd78cb0da9d5bb67e4d28d4dce725
Aman Verma <vaman644@gmail.com>
2021-12-06 12:16:30 +0530

    admin product list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,6 +15,7 @@ import PlaceOrderScreen from './screens/PlaceOrderScreen'
 import OrderScreen from './screens/OrderScreen'
 import UserListScreen from './screens/UserListScreen'
 import UserEditScreen from './screens/UserEditScreen'
+import ProductListScreen from './screens/ProductListScreen'
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
             <Route path="/cart/" element={<CartScreen />} />
             <Route path="/admin/userlist" element={<UserListScreen />} />
             <Route path="/admin/user/:id/edit" element={<UserEditScreen />} />
+            <Route path="/admin/productlist" element={<ProductListScreen />} />
             <Route path="/" element={<HomeScreen />} exact />
           </Routes>
         </Container>
